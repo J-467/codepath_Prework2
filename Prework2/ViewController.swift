7266141a850a4360bfef59217e0c250638bd5503
J-467 <jotweneboakodua@gmail.com>
2022-08-21 21:13:17 -0400

    Initial Commit

---
+++ Prework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prework2
+//
+//  Created by Julian Tweneboa Kodua on 8/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
